35134a810793f38f18efe28512d2b47f786cc986
whatf0xx <whatf0xx@gmail.com>
2019-05-21 11:57:35 +0100

    Update simulation1.py

--- simulation1.py
+++ simulation1.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat May 18 18:23:48 2019
-
 @author: whatf
 """
 
@@ -13,13 +12,13 @@ import seaborn as sns
 
 #%% define variables and functions
     #grid
-x = sp.linspace(-2, 2, 100)
-y = sp.linspace(-2, 2, 100)
+x = sp.linspace(-1.5, 1.5, 1000)
+y = sp.linspace(-1.5, 1.5, 1000)
 X, Y = sp.meshgrid(x, y)
 
     #convert to plane polars
 r = sp.sqrt(X**2 + Y**2)
-phi = sp.tan(Y/X)
+phi = sp.arctan2(Y, X)
 
     #set constants
 c_air = 343 #speed of sound in air (m/s)
@@ -29,21 +28,17 @@ f = 2000 #frequency of sound waves (Hz)
 omega = f*2*sp.pi
 k = omega/c_mat #wavenumber (/m)
 a = 1 #arbitrary cavity radius (m)
-m = 5
+m = 4
+
     #evaluate functions
 int_wgm_rho = spl.jv(m, n*r*k)
 ext_wgm_rho = spl.hankel1(m, r*k)
 B = spl.jv(m, n*a*k)/spl.hankel1(m, a*k)
-wgm_phi = sp.cos(m*phi)
-
+wgm_phi = sp.cos(m*phi) + sp.sin(m*phi)*sp.sqrt(-1)
+heavymap = sp.heaviside((r/a)-1, 1)
 #%% calculate values
-for i in range (0,len(X)):
-    for j in range (0, len(Y)):
-        if(r[i][j]<a):
-            wgm = wgm_phi*int_wgm_rho
-        else:
-            wgm = B*wgm_phi*ext_wgm_rho
+wgm = sp.real(wgm_phi*int_wgm_rho*(1-heavymap) + B*wgm_phi*ext_wgm_rho*heavymap)
 
 #%% make heatmap of function
-ax = sns.heatmap(wgm, linewidth=0.5)
+sns.heatmap(wgm)
 plt.show()
