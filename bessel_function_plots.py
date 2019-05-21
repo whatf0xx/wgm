71727545a59e51a9d02166394f349e5928e27e50
whatf0xx <whatf0xx@gmail.com>
2019-05-21 20:49:46 +0100

    Added circle that doesnt work yet

--- bessel_function_plots.py
+++ bessel_function_plots.py
@@ -11,9 +11,9 @@ import scipy.special as spl
 import matplotlib.pyplot as plt
 
 #%% create axes
-x = sp.linspace(20, 60, 5000) #Note for higher order Hankel functions exclude x values close to the origin
-n0 = 48 #Lower bound to generate
-n = 50 #Upper bound to generate
+x = sp.linspace(1, 15, 5000) #Note for higher order Hankel functions exclude x values close to the origin
+n0 = 7 #Lower bound to generate
+n = 9 #Upper bound to generate
 
 J = [spl.jv(n0, x)] #Bessel function, order 0; argument x
 for i in range(n0, n):
@@ -28,7 +28,7 @@ plt.subplot(2, 1, 1) #Bessel functions
 plt.title("Bessel functions")
 plt.grid(True)
 plt.vlines(0, -0.3, 0.3)
-plt.hlines(0, -1, 60)
+plt.hlines(0, -1, 15)
 for k in range(n0, n+1):
     plt.plot(x, J[k-n0], label="n=%d"% k)
 plt.legend()
@@ -38,7 +38,7 @@ plt.title("Hankel functions of the first kind")
 plt.legend()
 plt.grid(True)
 plt.vlines(0, -0.3, 0.3)
-plt.hlines(0, -1, 60)
+plt.hlines(0, -1, 15)
 for l in range(n0, n+1):
     plt.plot(x, H[l-n0], label="n=%d"% l)
 plt.legend()
--- simulation1.py
+++ simulation1.py
@@ -24,11 +24,11 @@ phi = sp.arctan2(Y, X)
 c_air = 343 #speed of sound in air (m/s)
 c_mat = 3500 #speed of sound in barrier material/outside cavity (m/s). Here the value used is for concrete
 n = c_mat/c_air #cavity refractive index
-f = 2000 #frequency of sound waves (Hz)
+f = 700 #frequency of sound waves (Hz)
 omega = f*2*sp.pi
 k = omega/c_mat #wavenumber (/m)
 a = 1 #arbitrary cavity radius (m)
-m = 4
+m = 9
 
     #evaluate functions
 int_wgm_rho = spl.jv(m, n*r*k)
@@ -36,9 +36,17 @@ ext_wgm_rho = spl.hankel1(m, r*k)
 B = spl.jv(m, n*a*k)/spl.hankel1(m, a*k)
 wgm_phi = sp.cos(m*phi) + sp.sin(m*phi)*sp.sqrt(-1)
 heavymap = sp.heaviside((r/a)-1, 1)
+
+    #draw on circle
+x_circ = sp.linspace(-a, a, 1000)
+y_circ_pos = sp.sqrt(a**2-x_circ**2)
+y_circ_neg = -sp.sqrt(a**2-x_circ**2)
+
 #%% calculate values
 wgm = sp.real(wgm_phi*int_wgm_rho*(1-heavymap) + B*wgm_phi*ext_wgm_rho*heavymap)
 
 #%% make heatmap of function
 sns.heatmap(wgm)
+plt.plot(x_circ, y_circ_pos, color='green')
+plt.plot(x_circ, y_circ_neg, color='green')
 plt.show()
