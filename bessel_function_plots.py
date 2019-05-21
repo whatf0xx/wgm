e3d3bdf32062f479142ae4d0ecf9a2ea46678829
whatf0xx <whatf0xx@gmail.com>
2019-05-21 13:06:34 +0100

    Discovered why Hankel functions weren't working around origin

--- bessel_function_plots.py
+++ bessel_function_plots.py
@@ -11,24 +11,24 @@ import scipy.special as spl
 import matplotlib.pyplot as plt
 
 #%% create axes
-x = sp.linspace(-1, 10, 50000)
-n0 = 0 #Lower bound to generate
-n = 4 #Upper bound to generate ('Hankel crashes at n=5?')
+x = sp.linspace(20, 60, 5000) #Note for higher order Hankel functions exclude x values close to the origin
+n0 = 48 #Lower bound to generate
+n = 50 #Upper bound to generate
 
 J = [spl.jv(n0, x)] #Bessel function, order 0; argument x
 for i in range(n0, n):
     J.append(spl.jv(i+1, x)) #Create Bessel functions, order 1-->n; argument x
 
-H = [spl.hankel1(n0, x)] #Hankel function, order 0; argument x
+H = [sp.real(spl.hankel1(n0, x))] #Hankel function, order 0; argument x
 for j in range(n0, n):
-    H.append(spl.hankel1(j+1, x)) #Create Hankel functions, order 1-->n; argument x
+    H.append(sp.real(spl.hankel1(j+1, x))) #Create Hankel functions, order 1-->n; argument x
 
 #%% plots
 plt.subplot(2, 1, 1) #Bessel functions
 plt.title("Bessel functions")
 plt.grid(True)
-plt.vlines(0, -0.5, 1.1)
-plt.hlines(0, -1, 10)
+plt.vlines(0, -0.3, 0.3)
+plt.hlines(0, -1, 60)
 for k in range(n0, n+1):
     plt.plot(x, J[k-n0], label="n=%d"% k)
 plt.legend()
@@ -37,8 +37,8 @@ plt.subplot(2, 1, 2) #Hankel functions
 plt.title("Hankel functions of the first kind")
 plt.legend()
 plt.grid(True)
-plt.vlines(0, -0.5, 1.1)
-plt.hlines(0, -1, 10)
+plt.vlines(0, -0.3, 0.3)
+plt.hlines(0, -1, 60)
 for l in range(n0, n+1):
     plt.plot(x, H[l-n0], label="n=%d"% l)
 plt.legend()
