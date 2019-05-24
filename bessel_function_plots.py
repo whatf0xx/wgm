35c503387783282fb7cbea4ef3b8021c03895d38
whatf0xx <whatf0xx@gmail.com>
2019-05-24 02:53:25 +0100

    Plot boundary condition as a graph to visualise roots; added a NR approximation to find eigenfrequencies
    Currently, neither really work but I have a good idea as to why the plot, at least, doesn't. The boundary condition relies on the Hankel function being dependent on a different k vector; otherwise it just equals the Bessel function when it has it's real part taken, hence there appear to be no non-trivial roots. I don't know why the Newton-Raphson method doesn't seem to work for the simulation, f'(x) seems to grow far too quickly leading to the adjustment being negligible.

--- bessel_function_plots.py
+++ bessel_function_plots.py
@@ -12,33 +12,56 @@ import matplotlib.pyplot as plt
 
 #%% create axes
 x = sp.linspace(1, 15, 5000) #Note for higher order Hankel functions exclude x values close to the origin
-n0 = 7 #Lower bound to generate
-n = 9 #Upper bound to generate
+n0 = 3 #Lower bound to generate
+n = 3 #Upper bound to generate
 
 J = [spl.jv(n0, x)] #Bessel function, order 0; argument x
 for i in range(n0, n):
     J.append(spl.jv(i+1, x)) #Create Bessel functions, order 1-->n; argument x
 
-H = [sp.real(spl.hankel1(n0, x))] #Hankel function, order 0; argument x
-for j in range(n0, n):
-    H.append(sp.real(spl.hankel1(j+1, x))) #Create Hankel functions, order 1-->n; argument x
+Jdash = [spl.jvp(n0, x, n=1)] #Bessel function first derivative, order 0; argument x
+for i in range(n0, n):
+    Jdash.append(spl.jvp(i+1, x, n=1)) #Create Bessel first derivatives, order 1-->n; argument x
+    
+H = [spl.hankel1(n0, x)] #Hankel function, order 0; argument x
+for i in range(n0, n):
+    H.append(spl.hankel1(i+1, x)) #Create Hankel functions, order 1-->n; argument x
 
+Hdash = [spl.h1vp(n0, x, n=1)] #Hankel function first derivative, order 0; argument x
+for i in range(n0, n):
+    Hdash.append(spl.h1vp(i+1, x, n=1)) #Create Hankel first derivatives, order 1-->n; argument x
+
+eigroot = [Jdash[0]/J[0] - Hdash[0]/H[0]]
+for i in range(0, n-n0):
+    eigroot.append(Jdash[i+1]/J[i+1] - Hdash[i+1]/H[i+1])
 #%% plots
-plt.subplot(2, 1, 1) #Bessel functions
+plt.subplot(3, 1, 1) #Bessel functions
 plt.title("Bessel functions")
 plt.grid(True)
 plt.vlines(0, -0.3, 0.3)
 plt.hlines(0, -1, 15)
-for k in range(n0, n+1):
-    plt.plot(x, J[k-n0], label="n=%d"% k)
+for i in range(n0, n+1):
+    plt.plot(x, J[i-n0], label="n=%d"% i)
 plt.legend()
     
-plt.subplot(2, 1, 2) #Hankel functions
+plt.subplot(3, 1, 2) #Hankel functions
 plt.title("Hankel functions of the first kind")
 plt.legend()
 plt.grid(True)
 plt.vlines(0, -0.3, 0.3)
 plt.hlines(0, -1, 15)
-for l in range(n0, n+1):
-    plt.plot(x, H[l-n0], label="n=%d"% l)
+for i in range(n0, n+1):
+    plt.plot(x, sp.real(H[i-n0]), label="n=%d"% i)
 plt.legend()
+
+plt.subplot(3, 1, 3) #Hankel functions
+plt.title("Newton-Raphson solutions for eigenmodes")
+plt.legend()
+plt.grid(True)
+plt.vlines(0, -0.3, 0.3)
+plt.hlines(0, -1, 15)
+for i in range(n0, n+1):
+    plt.plot(x, sp.real(eigroot[i-n0]), label="n=%d"% i)
+plt.legend()
+
+plt.tight_layout()
--- simulation1.py
+++ simulation1.py
@@ -24,11 +24,32 @@ phi = sp.arctan2(Y, X)
 c_air = 343 #speed of sound in air (m/s)
 c_mat = 3500 #speed of sound in barrier material/outside cavity (m/s). Here the value used is for concrete
 n = c_mat/c_air #cavity refractive index
-f = 700 #frequency of sound waves (Hz)
-omega = f*2*sp.pi
-k = omega/c_mat #wavenumber (/m)
 a = 1 #arbitrary cavity radius (m)
-m = 9
+m = 14
+f_trial = 6000 #frequency of sound waves (Hz)
+omega_trial = f_trial*2*sp.pi
+k_trial = omega_trial/c_mat #wavenumber (/m)
+k_guess = k_trial #so the original value can be compared to the computed one
+done = False
+counter = 0
+    #calculate eigenfrequencies
+while(done==False):
+    J = spl.jv(m, n*a*k_trial) #Bessel function at boundary (a)
+    Jdash = spl.jvp(m, n*a*k_trial, n=1) #first derivative
+    Jdodash = spl.jvp(m, n*a*k_trial, n=2) #second
+    H = spl.hankel1(m, a*k_trial) #Hankel function at boundary
+    Hdash = spl.h1vp(m, a*k_trial, n=1) #first derivative
+    Hdodash = spl.h1vp(m, a*k_trial, n=2) #second
+    
+    NRfunc = Jdash/J - Hdash/(n*H) #compute Newton-Raphson approximation for eigenfrequency
+    NRfuncdash = n*a*(Jdodash*J - 2*Jdash)/(J**2) - a*(Hdodash*H - 2*Hdash)/(n*H**2)
+    k_prev = k_trial
+    k_trial = k_trial - NRfunc/NRfuncdash
+    counter += 1
+    if(abs(k_trial-k_prev) < 0.01 and counter>5):
+        done=True
+
+k = k_trial #the computed eigenfrequency, corresponding to the first root
 
     #evaluate functions
 int_wgm_rho = spl.jv(m, n*r*k)
@@ -46,7 +67,11 @@ y_circ_neg = -sp.sqrt(a**2-x_circ**2)
 wgm = sp.real(wgm_phi*int_wgm_rho*(1-heavymap) + B*wgm_phi*ext_wgm_rho*heavymap)
 
 #%% make heatmap of function
-sns.heatmap(wgm)
-plt.plot(x_circ, y_circ_pos, color='green')
-plt.plot(x_circ, y_circ_neg, color='green')
-plt.show()
+g=sns.heatmap(wgm)
+g.set_xlabel("X")
+g.set_ylabel("Y")
+#sns.regplot(x_circ, y_circ_pos, scatter=False, color='green')
+#sns.regplot(x_circ, y_circ_neg, scatter=False, color='green')
+#plt.plot(x_circ, y_circ_pos, color='green')
+#plt.plot(x_circ, y_circ_neg, color='green')
+#plt.show()
